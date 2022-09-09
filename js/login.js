d038be04f5ff858fb53327ef6e1d23c239402fb5
nikimaz <112410562+nikimaz@users.noreply.github.com>
2022-09-09 11:16:19 +0300

    niki
    js

---
+++ js/login.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+  document.getElementById("login").addEventListener("click", function () {
+    document.getElementById("login").innerHTML = "logout";
+  });
+});
